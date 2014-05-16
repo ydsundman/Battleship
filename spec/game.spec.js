a985cbc13e2ae481fa81fc172c8444f787b59e84
Yassal Sundman <yassalsundman@gmail.com>
2014-05-16 13:31:10 +0200

    Simplify for now. We can add more ships when we need them.

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -4,31 +4,16 @@ define(['game', 'grid', 'ship'], function(Game, Grid, Ship) {
 			expect(typeof Game).toBe('function');
 		});
 
-		var shipExtractor = function(ships) {
-			var result = [], ship;
-			for (ship in ships) {
-				if (ships.hasOwnProperty(ship)) {
-					result.push(ships[ship]);
-				}
-			}
-			return result;
-		};
-
-		it('should be constructed with a Grid and a set of Ships', function() {
+		it('should be constructed with a Grid and a Ship', function() {
 			var game = new Game();
 			expect(game.constructor).toBe(Game);
 
 			expect(game.grid).toBeDefined();
 			expect(game.grid instanceof Grid).toBe(true);
 
-			expect(game.ships).toBeDefined();
-			expect(typeof game.ships).toBe('object');
-			var ships = shipExtractor(game.ships);
-			expect(ships.length).toBe(5);
-			ships.forEach(function(ship) {
-				expect(ship instanceof Ship).toBe(true);
-			});
-
+			expect(game.ship).toBeDefined();
+			expect(game.ship instanceof Ship).toBe(true);
+			expect(game.ship.size).toBe(5);
 		});
 	});
 });
