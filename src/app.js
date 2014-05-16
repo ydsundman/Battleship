24b14adb832dcb37f463e29ecd5c7298cc6ba84f
Yassal Sundman <yassalsundman@gmail.com>
2014-05-16 16:14:13 +0200

    PART1: First part of the app done, we have a grid, a ship and we're drawing them on the screen.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 /* global document:true */
 require(['game'], function(Game) {
+	var renderShip = function(ship){
+		var shipString = '<table><tr>';
+		for(var shipLength = 0; shipLength < ship.size; shipLength++){
+			shipString +='<td class="ship"></td>';
+		}
+		shipString += '</tr></table>';
+		return shipString;
+	};
+
+	var renderGrid = function(grid){
+		var gridString = '<table>';
+		for(var row = 0; row < grid.rowCount; row++){
+			gridString += '<tr>';
+			for(var col = 0; col < grid.columnCount; col++){
+				gridString +='<td></td>';
+			}
+			gridString += '</tr>';
+		}
+		gridString += '</table>';
+		return gridString;
+	};
+
 	var game = new Game();
 	var ships = document.getElementById('ships');
-	ships.innerHTML = ('Ship size: ' + game.ship.size);
+	ships.innerHTML = renderShip(game.ship);
 	var grid = document.getElementById('grid');
-	grid.innerHTML = ('Grid dimensions: ' + game.grid.columnCount + 'x' + game.grid.rowCount);
-
+	grid.innerHTML = renderGrid(game.grid);
 });
