89e3f5c474525d898891c23cb63f7290dd29386f
Daniel Sundman <daniel@yds.se>
2014-05-17 21:17:39 +0200

    Register attacks.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 /* global document:true */
 define('app', ['game','renderer'], function(game, renderer) {
-
-
 	var start = function() {
 		game.start();
 		renderer.renderGame(game);
+		registerAttackListener();
 	};
 	var registerStartButtonListener = function() {
 		var button = document.getElementById('startGame');
 		button.addEventListener('click', function() {
+			button.disabled = true;
 			start();
+		});
+	};
+	var registerAttackListener = function() {
+		var grid = document.querySelector('#grid table');
+		grid.addEventListener('click', function(e) {
+			e.target.classList.add('miss');
 		});
 	};
 	var init = function() {
